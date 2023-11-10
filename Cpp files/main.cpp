85cb4d466d126f90c14f6ea69a526a85113db28a
K Tarun Kumar Reddy <kallamtarun5@gmail.com>
2023-11-10 11:59:21 +0530

    Moved to Python

--- Cpp files/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <compression.h>
-
-using namespace std;
-
-int main()
-{
-
-}
--- Header files/decompression.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPRESSION_H_INCLUDED
-#define COMPRESSION_H_INCLUDED
-void gzip_decompression();
-void bzip_decompression();
-void tar_decompression();
-void zip_decompression();
-void rar_decompression();
-void sevenzip_decompression();
-#endif // COMPRESSION_H_INCLUDED
